d2467af99dd05a175d6541ed6b3805921abe5ea6
Isaiah Weaver <iswea1@morgan.edu>
2017-02-16 12:12:05 -0500

    experimented with background and named the app

--- index.ios.js
+++ index.ios.js
@@ -17,15 +17,9 @@ export default class smartEats extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Smart Eats 
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        
       </View>
     );
   }
@@ -36,7 +30,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#418EF7',
   },
   welcome: {
     fontSize: 20,
